779e8ec30152f4e1e0b4113955b7000df2728f0d
Jaimeson Waugh <jaimeson@4cesi.com>
2019-04-18 16:29:40 -0700

    Update PHPUnit version

--- tests/ComposerTemplateTest.php
+++ tests/ComposerTemplateTest.php
@@ -1,11 +1,13 @@
 <?php
 
+declare( strict_types = 1 );
+
 use PHPUnit\Framework\TestCase;
 use WaughJ\ComposerTemplate\ComposerTemplate;
 
 class ComposerTemplateTest extends TestCase
 {
-	public function testObjectWorks()
+	public function testObjectWorks() : void
 	{
 		$object = new ComposerTemplate();
 		$this->assertTrue( is_object( $object ) );
